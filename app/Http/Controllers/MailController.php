c25e14bb0f162a6e4707285d240dfe882b822055
EduardoCortezB <eduardocortezzeus@gmail.com>
2022-08-13 22:03:54 -0500

    email form v7

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -16,12 +16,7 @@ class MailController extends Controller
             'title'=>'required',
             'body'=>'required',
         ]);
-        $details = [
-            'from'=>$request->get('from'),
-            'name'=>$request->get('name'),
-            'title'=>$request->get('title'),
-            'body'=>$request->get('body'),
-        ];
+
         \Mail::send('mail.contactFormAdmin', array(
             'name' => $request->get('name'),
             'email' => $request->get('from'),
@@ -33,7 +28,7 @@ class MailController extends Controller
         });
 
         // Mail::to('celutsystem@gmail.com')->queue(new MailForm($details));
-        // $sessionManager->flash('message', 'Se ha enviado el correo exitosamente');
+        $sessionManager->flash('message', 'Se ha enviado el correo exitosamente');
         return back();
     }
 }
--- resources/views/mail/contactFormAdmin.blade.php
+++ resources/views/mail/contactFormAdmin.blade.php
@@ -1,8 +1,8 @@
-<h2>Hello</h2> <br><br>
-You have got an email from : {{ $name }} <br><br>
-User details: <br><br>
-Name: {{ $name }} <br>
+<h2>Email desde formulario de contacto</h2> <br><br>
+Mensaje enviado por: {{ $name }} <br><br>
+Detalles <br><br>
+Nombre: {{ $name }} <br>
 Email: {{ $email }} <br>
-Subject: {{ $subject }} <br>
-Message: {{ $user_query }} <br><br>
-Thanks
+Titulo: {{ $subject }} <br>
+Mensaje: {{ $user_query }} <br><br>
+No responder a este correo, ya que fue enviado por el sistema y ejecutado a un evento realizado por el usuario.
